6f6db9955e0c7f7ed5ed823a893c8005cf97c404
Sagamir <Sagamir@users.noreply.github.com>
2018-03-28 12:27:30 +0200

    Update python.py
    added comments

--- python.py
+++ python.py
@@ -5,17 +5,23 @@ import serial
 import time
 import irsdk
 
+# Read the config file
 config = configparser.ConfigParser(inline_comment_prefixes=';')
 config.read('config.ini')
 
-
+# Building up the Serial Connection to the Arduino
 ser = serial.Serial(config['config']['COMPort'], config['config']['BaudRate'], timeout=int(config['config']['COMTimeout']))
 
+
+#########################
+###  Demo Mode        ###
+#########################
+
+# See if demo mode is active
 demo = eval(config['config']['demoMode'])
 demoShow = True
 
 if(demo):
-    #print(config['porsche919']['fuelAlarm'])
     while True:
         if demoShow == True:
             leds = "rgbkycmykrgb#"
@@ -26,11 +32,21 @@ if(demo):
         ser.write(leds.encode())
         time.sleep(.2)
 
+		
+#########################
+###  Initialisation   ###
+#########################
+
+# Initialize global variables
 class State:
     ir_connected = False
     rpm_shift_show = 0
     fuel_show = 0
+	trackName = ""
+	
 
+	
+# Check if iRacing is running
 def check_iracing():
     if state.ir_connected and not (ir.is_initialized and ir.is_connected):
         state.ir_connected = False
@@ -39,17 +55,28 @@ def check_iracing():
     elif not state.ir_connected and ir.startup() and ir.is_initialized and ir.is_connected:
         state.ir_connected = True
         print('irsdk connected')
+		state.trackName = ir['WeekendInfo']['TrackDisplayName']
+		
+		
+		
+##########################
+### The mighty Program ###
+##########################
 def loop():
+	# Freeze the buffer to not get any 2 different values because the API updated too soon
     ir.freeze_var_buffer_latest()
 
-
-    #rpm = ir['RPM']
-    fuelPct = ir['FuelLevelPct'] * 100
-    #leds = "kkkkkkkkkkkk#"
-    leds = ""
-
-    deltaTime = int(round(ir[config['config']['DeltaTo']],1)*10)
+	# variables that always get reset
+    fuelPct = ir['FuelLevelPct'] * 100		# fuel in percentage
+    leds = ""								# reset leds
+    deltaTime = int(round(ir[config['config']['DeltaTo']],1)*10)	# get deltaTime, truncate it, round it, convert to int
     
+	
+	#############################
+	### Delta Time            ###
+	#############################
+	
+	# if you are faster than the compared laptime
     if deltaTime < 0:
         i = 0
         while i > deltaTime:
@@ -59,6 +86,7 @@ def loop():
             else:
                 leds += "g"
                 i -= 1
+	# if you are slower than the compared laptime - noob
     if deltaTime > 0:
         i = 0
         while i < deltaTime:
@@ -68,12 +96,20 @@ def loop():
             else:
                 leds += "r"
                 i += 1
+				
+	# well, if the deltatime is 0, show nothing
     if deltaTime == 0:
         leds = "kkkkkkkkkkkk"
+		
+	# put as many "k" behind the leds String to get to 12 characters
     while len(leds) < 12:
         leds += "k"
-    leds += "#"
 
+		
+		
+	#############################
+	### RPM                   ###
+	#############################
     """
     startRPM = 5000
     endRPM = 6500
@@ -115,19 +151,32 @@ def loop():
         else:
             state.rpm_shift_show = 0
     """
+	
+	#############################
+	### Fuel Alarm            ###
+	#############################
+	
+	# Show only if the fuel is under the desired fuelAlarm threshold	
     if(fuelPct <= config['default']['fuelAlarm']):
+		# let the led flash magenta
         if(state.fuel_show <= 10):
-            leds = 'm' + leds[1:]
+            leds = leds[:-1] + "m"
             state.fuel_show = state.fuel_show + 1
         elif(state.fuel_show > 10) and (state.fuel_show <= 20):
-            leds = 'k' + leds[1:]
+            leds = leds[:-1] + "k"
             state.fuel_show = state.fuel_show + 1
         else:
             state.fuel_show = 0
 
+			
+	#############################
+	### End Stuff             ###
+	#############################
+    leds += "#"					# add the end character, to show the arduino that its the end of the string
+    ser.write(leds.encode())	# Send the leds string to the arduino
 
-    ser.write(leds.encode())
 
+	
 
 if __name__ == '__main__':
     ir = irsdk.IRSDK()
@@ -138,6 +187,6 @@ if __name__ == '__main__':
             check_iracing()
             if state.ir_connected:
                 loop()
-            time.sleep(.05)
+            time.sleep(.05)		# how often the loop will execute per second
     except KeyboardInterrupt:
         pass
